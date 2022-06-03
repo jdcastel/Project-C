f26096004e6f72940f745518c491b98bf5df7bcb
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2022-06-03 14:17:27 -0400

    Last Commit

--- MS4/contacts.c
+++ MS4/contacts.c
@@ -1,24 +1,8 @@
-//==============================================
-// Name:           Juan David Rodriguez Castelblanco
-// Student Number: 147891204
-// Email:          jdrodriguez-castelbl@myseneca.ca
-// Section:        NFF
-// Date:           24/07/2021
-//==============================================
-// Assignment:     2
-// Milestone:      4
-//==============================================
+
 
 #define _CRT_SECURE_NO_WARNINGS
 
-// +-----------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-2     |
-// |        function definitions below...                |
-// |                                                     |
-// | HINT:  Update the getNumbers function to use the    |
-// |        new helper function "getTenDigitPhone"       |
-// |        where applicable                             |
-// +-----------------------------------------------------+
+
 
 #include <stdio.h>
 #include "contacts.h"
